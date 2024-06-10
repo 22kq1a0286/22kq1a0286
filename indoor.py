c07d2dc034405b06594402e9a1faef38173316b5
22kq1a0286 <22kq1a0286@pace.ac.in>
2024-06-10 14:26:22 +0530

    Create indoor.py

---
+++ indoor.py
@@ -0,0 +1,12 @@
+def main():
+    user_input = input("Enter some text: ")
+    lowercase_text = user_input.lower()
+    print(lowercase_text)
+
+if __name__ == "__main__":
+    main()
+
+<!---
+22kq1a0286/22kq1a0286 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
